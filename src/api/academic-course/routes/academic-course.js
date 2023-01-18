e76532f622234cd320afd0cdd7178e11d2e31821
Anurag Pal <apal895@gmail.com>
2023-01-18 23:48:45 +0530

    posstgres setup doen

--- src/api/academic-course/routes/academic-course.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * academic-course router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::academic-course.academic-course');
